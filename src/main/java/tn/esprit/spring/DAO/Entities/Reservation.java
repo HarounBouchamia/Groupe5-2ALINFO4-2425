30f050f349e43c908934edfaa6d2a59c626e9b21
Haroun Bouchamia <harounbouchamia123@gmail.com>
2025-04-11 14:22:12 +0100

    third commit

--- src/main/java/tn/esprit/spring/DAO/Entities/Reservation.java
+++ src/main/java/tn/esprit/spring/DAO/Entities/Reservation.java
@@ -1,4 +1,30 @@
 package tn.esprit.spring.DAO.Entities;
 
-public class Reservation {
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "T_RESERVATION")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Reservation implements Serializable {
+    @Id
+    String idReservation;
+    LocalDate anneeUniversitaire;
+    boolean estValide;
+    @ManyToMany
+    @JsonIgnore
+    List<Etudiant> etudiants = new ArrayList<>();
+
 }
